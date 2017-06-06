9327497314a7a4575d17818011ebd90a40e05cd4
I. Dan Calinescu <idancalinescu@gmail.com>
2017-06-06 19:40:02 +0300

    added on mount event

--- screens/details.js
+++ screens/details.js
@@ -10,8 +10,8 @@ import { Styles, Screen } from 'react-native-chunky'
 export default class ItemDetailsScreen extends Screen {
 
   render() {
-    return (<Card title={this.props.data().title}>
-        <Text> {this.props.data().details || 'Details coming soon'} </Text>
+    return (<Card title={'Details'}>
+        <Text>{'Details coming soon'}</Text>
     </Card>)
   }
 }
